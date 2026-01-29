daac8430e6b2cb99aa0926adde3bb673dc24af5e
The179UCETile <khang240582@gmail.com>
2026-01-29 21:01:08 +0700

    e

--- indexscript.js
+++ indexscript.js
@@ -1,10 +1,5 @@
-const tickermsg = ["hi", "tw.nw119.me ~ I&apos;m a furry", "dominoguy_ uses AI for coding (effectively vibecoding", "No RenaultFan Delete RenaultFan Yes haster Add haster", "No jQuery was used on this news ticker"];
-let elem = document.getElementById("tickermessage");
+const tickermsg = ["hi", "tw.nw119.me ~ I&apos;m a furry", "dominoguy_ uses AI for coding (effectively vibecoding)", "No RenaultFan Delete RenaultFan Yes haster Add haster", "No jQuery was used on this news ticker"];
 function updateTicker() {
-  elem.style.color = "rgba(255,255,255,0)";
-  setTimeout(()=>{
-    elem.innerHTML = tickermsg[Math.floor(Math.random()*tickermsg.length)];
-    elem.style.color = "rgba(255,255,255,1)";
-  }, 1000);
+  document.getElementById("tickermessage").innerHTML = tickermsg[Math.floor(Math.random()*tickermsg.length)];
 }
 setInterval(updateTicker, 10000);
