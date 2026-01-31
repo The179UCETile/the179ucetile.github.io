b3209cc2d374c2c4c86f06e0f6892605fd7c8ff0
The179UCETile <khang240582@gmail.com>
2026-01-31 20:29:14 +0700

    fix

--- other/punishmentGen/generator.js
+++ other/punishmentGen/generator.js
@@ -137,8 +137,8 @@ function generatePunishment() { try {
   let punishmentInfo = punishmentsSorted[diffRange][Math.floor(Math.random()*(punishmentsSorted[diffRange]??["a"]).length)];
   if (punishmentInfo[1] >= 8) {
     utils.playAudio("emojideath");
-  } else if (punishmentInfo[1] >= 4) {
-    utils.playAudio("vineBoomBassboost")
+  } else if (punishmentInfo[1] >= 6) {
+    utils.playAudio("vineBoomBassboost");
   } else if (punishmentInfo[1] >= 4) {
     utils.playAudio("vineBoom");
   } else {};
