84a7ca43c113ef73f1c916a0738745437902accc
The179UCETile <khang240582@gmail.com>
2026-01-31 14:45:01 +0700

    fix

--- other/punishmentGen/generator.js
+++ other/punishmentGen/generator.js
@@ -30,19 +30,19 @@ function noUndef(value) {
   return !(value===undefined);
 }
 const utils = {
-  processModif: function(type, arr) {
+  processModif: function(type) {
     switch (type) {
       case "mult": {
         let out = 1;
-        for (let i = 0; i < arr.length&&arr[i][0]; i++) {
-          out *= arr[i][1];
-        }
+        for (let i = 0; i < modif.length&&modif[i][0]; i++) {
+          out *= modif[i][1];
+        };
         return out;
         } break;
     }
   },
-  properNum: function(base, arr, integ = false) {
-    let out = base*this.processModif("mult", arr);
+  properNum: function(base, integ = false) {
+    let out = base*this.processModif("mult", modif);
     if (integ) {
       out = Math.floor(out);
     };
@@ -64,8 +64,8 @@ const utils = {
       return portions[0];
     return portions[0] + "." + portions[1];
   },
-  properDiff: function(num, arr) {
-    return Number((num+Math.log10(this.processModif("mult", arr))/Math.log10(3)).toFixed(2));
+  properDiff: function(num, modif) {
+    return Number((num+Math.log10(this.processModif("mult", modif))/Math.log10(3)).toFixed(2));
   }
 }
 function generatePunishment() { try {
@@ -79,17 +79,18 @@ function generatePunishment() { try {
   const Punishments = [
     // punishmentName, diff, defaultDiff, modif (optional)
     ["Nothing happens.", 0, 0],
-    [`Scroll ${utils.properNum(1e3,modif)} characters in TextWall.<br>Page zooming is forbidden.`, utils.properDiff(1.2,modif), 1.2],
+    [`Scroll ${utils.properNum(1e3)} characters in TextWall.<br>Page zooming is forbidden.`, utils.properDiff(1.2), 1.2],
     [`Survive the fictional googology punishment.`, 1.6, 1.6],
-    [`Scroll ${utils.properNum(5e3,modif)} characters in TextWall.<br>Page zooming is forbidden.`, utils.properDiff(3,modif), 3],
-    [`Walk ${utils.properNum(100,modif)} meters IRL.`, utils.properDiff(1.4,modif), 1.4],
+    [`Scroll ${utils.properNum(5e3)} characters in TextWall.<br>Page zooming is forbidden.`, utils.properDiff(3), 3],
+    [`Walk ${utils.properNum(100)} meters IRL.`, utils.properDiff(1.4), 1.4],
     ["Actually touch grass.<br>The hard part is finding grass.", 2.6, 2.6],
-    [`Drink ${utils.properNum(25,modif)}ml of water.`, utils.properDiff(1.5,modif), 1.5],
-    [`Scroll ${utils.properNum(25e3,modif)} characters in TextWall.<br>Page zooming is forbidden.`, utils.properDiff(4.8,modif), 4.8],
-    [`In any game that you have experience on, complete ${utils.properNum(2,modif)} levels that you consider the closest to &quot;Medium&quot; in Eternal Towers of Hell difficulty.`, utils.properDiff(2.44,modif), 2.44],
+    [`Drink ${utils.properNum(25)}ml of water.`, utils.properDiff(1.5), 1.5],
+    [`Scroll ${utils.properNum(25e3)} characters in TextWall.<br>Page zooming is forbidden.`, utils.properDiff(4.8), 4.8],
+    [`In any game that you have experience on, complete ${utils.properNum(2)} levels that you consider the closest to &quot;Medium&quot; in Eternal Towers of Hell difficulty.`, utils.properDiff(2.44), 2.44],
     ["Regenerate, and double the next punishment.", 4.15, 4.15, ["mult", 2, 2]],
     ["Regenerate, and triple the next punishment.", 4.86, 4.86, ["mult", 3, 2]],
-    [`For the next ${utils.properNum(15,modif)} minutes, type your messages .drawkcab`, utils.properDiff(5.32,modif), 5.32]
+    [`For the next ${utils.properNum(15)} minutes, type your messages .drawkcab`, utils.properDiff(5.32), 5.32],
+    [`For the next ${utils.properNum(10)} minutes, speak with only emojis.`, utils.properDiff(5.48), 5.48]
   ];
   for (let i = 0; i < Punishments.length; i++) {
     if (punishmentsSorted[Math.floor(Punishments[i][2])]) {
