f547e941fdefd64e21037d8c508927c96643161d
The179UCETile <khang240582@gmail.com>
2026-02-16 14:59:43 +0700

    e

--- other/canvasTesting/canvasScript.js
+++ other/canvasTesting/canvasScript.js
@@ -17,6 +17,9 @@ function render(id) {
       ctx.beginPath();
       ctx.moveTo(mapX(-1), mapY(0));
       for (let i = 0; i <= 50; i++) {
+        if (i == 0) {
+          ctx.moveTo(mapX(-1), mapY(Math.sin(t * Math.PI * 2) / 2))
+        };
         ctx.lineTo(mapX(i / 25 - 1), mapY(Math.sin((i / 25 + t) * Math.PI * 2) / 2))
       };
       ctx.lineWidth = 5;
