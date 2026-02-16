819f33a539482ce4fb5b25c271d74cfc7cb97a94
The179UCETile <khang240582@gmail.com>
2026-02-16 14:29:32 +0700

    e

---
+++ other/canvasTesting/canvasScript.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let index = 0;
+const ts = Date.now();
+ctx.translate(canvas.width / 2, canvas.height / 2);
+function mapX(expectX) {
+  return expectX / 2 * canvas.width
+}
+function mapY(expectY) {
+  return expectY / 2 * canvas.height
+}
+function render(id) {
+  const t = (Date.now() - ts) / 1e3;
+  switch (id) {
+    case 0: {
+      ctx.beginPath();
+      ctx.moveTo(mapX(-1), mapY(0));
+      for (let i = 0; i <= 50; i++) {
+        ctx.lineTo(mapX(i / 25 - 1), mapY((i / 25 + t) * Math.PI * 2))
+      };
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = "#ffffff";
+      ctx.stroke()
+    }; break;
+  }
+};
+setInterval(()=>{render(index)})
