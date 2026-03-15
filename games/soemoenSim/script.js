62b7451cea27024d9ca59cf5e35f1f5d133665e1
The179UCETile <khang240582@gmail.com>
2026-03-15 20:06:05 +0700

    e

--- games/soemoenSim/script.js
+++ games/soemoenSim/script.js
@@ -147,7 +147,7 @@ function updateEffects() {
     upgEffect.begUpg.upg2 = new Decimal("1.4").pow(s.begUpg2Bought)
   };
   upgEffect.begUpg.upg3 = s.begUpg3Bought.div("100");
-  upgEffect.begUpg.upg4 = s.begUpg4Bought.gte("1") ? s.itemsBegged.log10().pow("0.7").add("1") : new Decimal("1")
+  upgEffect.begUpg.upg4 = s.begUpg4Bought.gte("1") ? s.itemsBegged.add("1").log10().pow("0.7").add("1") : new Decimal("1")
 }
 function save() {
   localStorage.setItem("saveSoemoenSim", JSON.stringify(s))
