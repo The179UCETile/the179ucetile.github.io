a43f12c76144bf3c4826dfe67951ab0455f1a2d2
The179UCETile <khang240582@gmail.com>
2026-03-17 20:15:40 +0700

    e

---
+++ games/rhabk_game/script.js
@@ -0,0 +1,29 @@
+// does rhabk understand??
+let audio = document.getElementById("gameTheme");
+audio.loop = true;
+audio.preload = true;
+function playAudio() {
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play()
+}
+const t = Date.now();
+function update() {
+  const timeLeft = (864e5 - (Date.now() - t)) / 1e3;
+  setInterval(()=>{
+    if (timeLeft < 0) {
+      document.getElementById("fakeGame").style.display = "none";
+      document.getElementById("win").style.display = "";
+    } else {
+      document.getElementById("chapter").innerHTML = `YOUR CURRNTLY ON CHALPER ${Math.floor(timeLeft / 28800) + 1}`;
+      document.getElementById("timeLeft").innerHTML = `${timeLeft} secknds lft!!!!`
+    }
+  }, 16)
+}
+document.getElementById("rhabk is trash").addEventListener("click", function () {
+  t = Date.now();
+  document.getElementById("titleScreen").style.display = "none";
+  document.getElementById("fakeGame").style.display = "";
+  playAudio();
+  update()
+})
