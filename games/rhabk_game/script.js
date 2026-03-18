b709de58defeaaad6e0985fbe558820663ea49d8
The179UCETile <khang240582@gmail.com>
2026-03-18 17:51:01 +0700

    e

--- games/rhabk_game/script.js
+++ games/rhabk_game/script.js
@@ -2,10 +2,20 @@
 let audio = document.getElementById("gameTheme");
 audio.loop = true;
 audio.preload = true;
+for (let i = 1; i < 8; i++) {
+  let A = document.getElementById(`sfx${i}`);
+  A.preload = true;
+}
 function playAudio() {
   audio.pause();
   audio.currentTime = 0;
   audio.play()
+}
+function playSFX(num) {
+  let A = document.getElementById(`sfx${i}`);
+  A.pause();
+  A.currentTime = 0;
+  A.play()
 }
 let t = Date.now();
 function update() {
@@ -15,6 +25,9 @@ function update() {
       document.getElementById("fakeGame").style.display = "none";
       document.getElementById("win").style.display = "";
     } else {
+      if (Math.random() < 10 ** (-7 + Math.floor((86400 - timeLeft) / 28800) + 1)) {
+        playSFX(Math.floor(Math.random() * 7 + 1))
+      }
       document.getElementById("chapter").innerHTML = `YOUR CURRNTLY ON CHALPER ${Math.floor((86400 - timeLeft) / 28800) + 1}`;
       document.getElementById("timeLeft").innerHTML = `${timeLeft} secknds lft!!!!`
     }
