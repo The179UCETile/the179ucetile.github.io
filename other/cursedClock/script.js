e8521cf6fc1722dd8f096600d294c05c31fba3b9
The179UCETile <khang240582@gmail.com>
2026-03-25 20:46:50 +0700

    e

--- other/cursedClock/script.js
+++ other/cursedClock/script.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 ctx.font = "30px argam";
 ctx.translate(canvas.width / 2, canvas.height / 2);
+ctx.fillStyle = "#fff";
 function mapX(expectX) {
   return expectX / 2 * canvas.width
 }
@@ -12,7 +13,6 @@ function mapY(expectY) {
 function getArgamTime(obj) {
   return `${argamChars[obj.getHours()]}:${argamChars[obj.getMinutes()]}:${argamChars[obj.getSeconds()]}`
 }
-function $(id) { return document.getElementById(id) }
 function update() {
   const time = new Date();
   ctx.clearRect(mapX(-1), mapY(-1), canvas.width, canvas.height);
