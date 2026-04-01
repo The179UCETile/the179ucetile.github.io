2a9d5ce08df293fb9e343823b1ace8880e0c6c8f
The179UCETile <khang240582@gmail.com>
2026-04-01 18:45:30 +0700

    e

--- indexscript.js
+++ indexscript.js
@@ -18,7 +18,7 @@ function updateTicker() {
   setTimeout(()=>{
     document.getElementById("tickermessage").innerHTML = tickermsg[Math.floor(Math.random()*tickermsg.length)];
     document.getElementById("tickermessage").style.color = "rgba(255,255,255,1)";
-  },10);
+  },200);
 }
 setInterval(updateTicker, 1000);
 document.getElementById("tickermessage").innerHTML = tickermsg[Math.floor(Math.random()*tickermsg.length)];
