1fa9feef32c6e110564c47b7a57003a5932b7a07
The179UCETile <khang240582@gmail.com>
2026-04-02 21:07:51 +0700

    e

--- other/spellingBee/script.js
+++ other/spellingBee/script.js
@@ -9,39 +9,38 @@ function doTheThing(data) {
   const words = data.split("\n");
   let timestamp = Date.now();
   let score = 0;
-  let g = document.getElementById;
   let word = words[Math.floor(Math.random() * words.length)];
   function check() {
-    if (g("wordInput").value == word) {
+    if (document.getElementById("wordInput").value == word) {
       word = words[Math.floor(Math.random() * words.length)];
-      g("wordInput").value = "";
+      document.getElementById("wordInput").value = "";
       timestamp = Date.now();
     } else {
       endGame();
     }
   }
   function endGame() {
-    g("gameover").style.display = "";
-    g("main").style.display = "none";
+    document.getElementById("gameover").style.display = "";
+    document.getElementById("main").style.display = "none";
   }
   function update() {
-    g("word").innerHTML = `<p>${word}</p>`;
-    g("word").style.filter = `blur(${Math.sqrt(score) * .3}px)`;
+    document.getElementById("word").innerHTML = `<p>${word}</p>`;
+    document.getElementById("word").style.filter = `blur(${Math.sqrt(score) * .3}px)`;
   }
   function startGame() {
     word = words[Math.floor(Math.random() * words.length)];
     score = 0;
-    g("main").style.display = "";
-    g("gameOver").style.display = "none";
-    g("wordInput").value = "";
+    document.getElementById("main").style.display = "";
+    document.getElementById("gameOver").style.display = "none";
+    document.getElementById("wordInput").value = "";
     timestamp = Date.now();
   }
-  g("wordInput").addEventListener("keydown", (e) => {
+  document.getElementById("wordInput").addEventListener("keydown", (e) => {
     if (e.code == "Enter") {
       check();
     }
   });
-  g("retry").addEventListener("click", startGame);
-  g("check").addEventListener("click", check);
+  document.getElementById("retry").addEventListener("click", startGame);
+  document.getElementById("check").addEventListener("click", check);
   setInterval(update, 16)
 }
