30992eb6d645810bfc9e651c01c7fb7deba46e8e
The179UCETile <khang240582@gmail.com>
2026-04-02 20:56:51 +0700

    e

--- other/spellingBee/script.js
+++ other/spellingBee/script.js
@@ -43,6 +43,7 @@ function doTheThing(data) {
   });
   g("retry").addEventListener("click", () => {
     startGame();
-  })
+  });
+  g("check").addEventListener("click", check)
   setInterval(update, 16)
 }
