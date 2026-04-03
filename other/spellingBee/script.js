ff21e649fa45e197d51dc4a4f160b4e3ebe149a6
The179UCETile <khang240582@gmail.com>
2026-04-03 20:49:05 +0700

    e

--- other/spellingBee/script.js
+++ other/spellingBee/script.js
@@ -12,13 +12,20 @@ Add difficulties
 function doTheThing(data) {
   const words = data.split("\n");
   let timestamp = Date.now();
+  let gameStartTimestamp = Date.now();
   let score = 0;
   let word = words[Math.floor(Math.random() * words.length)];
+  let totalChars = 0;
+  function formatTime(s) {
+    if (s < 3600) return `${Math.floor(s / 60 + 100).slice(1)}:${Math.floor(s % 60 + 100).slice(1)}`;
+    return `${Math.floor(s / 3600 + 100).slice(1)}:${Math.floor((s / 60) % 60 + 100).slice(1)}:${Math.floor(s % 60 + 100).slice(1)}`
+  }
   function getTime(score, min, start, pow, speed) {
     return Math.max(min, start / (score * speed + 1) ** pow)
   }
   function check() {
     if (document.getElementById("wordInput").value == word) {
+      totalChars += word.length;
       word = words[Math.floor(Math.random() * words.length)];
       document.getElementById("wordInput").value = "";
       timestamp = Date.now();
@@ -31,6 +38,7 @@ function doTheThing(data) {
   function endGame() {
     document.getElementById("gameover").style.display = "";
     document.getElementById("main").style.display = "none";
+    document.getElementById("gameoverInfo").innerHTML = `Your score was: ${score.toLocaleString("en-US")}<br>Average WPM: ${Math.floor(totalChars / ((Date.now() - gameStartTimestamp) / 1e3) / 5 * 60)}<br>You survived for ${formatTime((Date.now() - gameStartTime) / 1e3)}.`;
   }
   function update() {
     let timeRemain = getTime(score, 0.2, 2, 1.2, .05) * 1e3 * word.length;
