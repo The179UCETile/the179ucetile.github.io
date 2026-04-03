1a8a71809167954bcc806bfd10bdc4a2087417c7
The179UCETile <khang240582@gmail.com>
2026-04-03 18:53:28 +0700

    e

--- other/spellingBee/script.js
+++ other/spellingBee/script.js
@@ -5,11 +5,18 @@ try {
 } catch (e) {
   alert(e.stack)
 }
+/*
+TODO:
+Add difficulties
+*/
 function doTheThing(data) {
   const words = data.split("\n");
   let timestamp = Date.now();
   let score = 0;
   let word = words[Math.floor(Math.random() * words.length)];
+  function getTime(score, min, start, pow, speed) {
+    return Math.max(min, start / (score * speed + 1) ** pow)
+  }
   function check() {
     if (document.getElementById("wordInput").value == word) {
       word = words[Math.floor(Math.random() * words.length)];
@@ -26,8 +33,11 @@ function doTheThing(data) {
     document.getElementById("main").style.display = "none";
   }
   function update() {
+    let timeRemain = getTime(score, 1, 10, 1.2, .05);
     document.getElementById("word").innerHTML = `<p>${word}</p>`;
-    document.getElementById("word").style.filter = `blur(${Math.sqrt(score) * .3}px)`;
+    document.getElementById("word").style.filter = `blur(${Math.sqrt(score) * .5}px)`;
+    document.getElementById("timeRemaining").innerHTML = `${((timeRemain * 1e3 - (Date.now() - timestamp)) / 1e3).toFixed(2)}s`;
+    if (timeRemain * 1e3 - (Date.now() - timestamp) < 0) endGame()
   }
   function startGame() {
     word = words[Math.floor(Math.random() * words.length)];
