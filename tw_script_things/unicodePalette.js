458d8616b4d97ca97242b4f01f44096a45c2beea
The179UCETile <khang240582@gmail.com>
2026-06-10 20:40:06 +0700

    Update unicodePalette.js

--- tw_script_things/unicodePalette.js
+++ tw_script_things/unicodePalette.js
@@ -28,23 +28,54 @@ style.innerHTML = `
 }
 #charContainer {
   position: absolute;
-  display: grid;
-  grid-template-columns: repeat(15, 1fr);
-  overflow: auto;
-  gap: 5px;
-  height: 175px;
+  height: 165px;
   width: 100%;
   background-color: #333e;
   border: 2px solid #555;
   top: 0px;
+  padding: 5px;
   border-radius: 0 0 25px 25px;
 }
-#charContainer button {
+#chars {
+  position: absolute;
+  top: 30px;
+  width: calc(100% - 10px);
+  height: 135px;
+  display: grid;
+  overflow: auto;
+  grid-template-columns: repeat(15, 1fr);
+  place-content: start center;
+  gap: 5px;
+}
+#chars button {
   border: 2px solid #222;
   border-radius: 4px;
   background-color: #000;
   color: #fff;
-  font-size: 1.1em;
+  padding: 2px;
+  text-align: center;
+  height: 30px;
+}
+.groupContainer {
+  position: absolute;
+  width: calc(100% - 10px);
+  top: 0px;
+  height: 25px;
+  display: grid;
+  grid-template-columns: 40px auto 40px;
+  gap: 5px;
+}
+.groupContainer button {
+  background-color: #222;
+  border: 2px solid #444;
+  border-radius: 4px;
+  color: #fff;
+  padding: 2px;
+}
+.groupContainer div {
+  background-color: #444;
+  border: 2px solid #888;
+  border-radius: 4px;
   padding: 2px;
   text-align: center;
 }
@@ -62,13 +93,47 @@ closeBtn.addEventListener("click", () => {
 });
 let charContainer = document.createElement("div");
 charContainer.id = "charContainer";
-const characters = "█■▔🭶🭷🭸🭹🭺🭻▁🮀🮁🮗▐▌▞▚▛▀▜▙▄▟▗▖▝▘🮕🮖░▒▓🮐🮒🮑🮘🮙▕🮇🮈🮉🮊🮋▉▊▋▍▎▏▂▃▅▆▇🮆🮅🮄🮃🮂🮞🮟🮝🮜🭈🭆🭂🭍🭑🬽🭣🭧🭓🭞🭜🭘🭊🭁🭇🭄🭏🬼🭌🬿🭥🭒🭢🭕🭠🭗🭝🭚◢◣◥◤🭃🭉🬾🭎🭔🭤🭙🭟🭅🭋🭀🭐🭖🭦🭛🭡🭮🭩🭯🭨🮛🭪🭬🭫🮚🭭🬞🬭🬏🬇🬋🬃🬁🬂🬀🬠🬰🬐🬦🬹🬓🬉🬎🬄🬙🬥🬖🬢🬔🬧🬡🬒🬜🬪🬶🬳🬈🬅🬣🬘🬯🬮🬛🬫🬴🬸🬤🬗🬕🬨🬚🬩🬝🬬🬵🬱🬲🬷🬌🬍🬺🬻🬊🬆━┃╺╸╻╹═║╌┄┈╍┅┉╎┆┊╏┇┋┍┯┑┝┿┥┕┷┙╒╤╕╞╪╡╘╧╛╓╥╖╟╫╢╙╨╜╔╦╗╠╬╣╚╩╝╲╳╱🮭🮬🮫🮪🮣🮢🮡🮠🮦🮥🮮🮤🮧🮩🮨".split(/(?:)/u);
-for (let i in characters) {
-  let btn = document.createElement("button");
-  btn.addEventListener("click", ()=>{writeChar(characters[i], 1)});
-  btn.innerHTML = characters[i];
-  charContainer.appendChild(btn)
-};
+charContainer.innerHTML = `
+<div class="groupContainer">
+  <button id="prevGroup">←</button>
+  <div id="groupName">???</div>
+  <button id="nextGroup">→</button>
+</div>
+<div id="chars"></div>
+`;
 unicodePal.appendChild(closeBtn);
 unicodePal.appendChild(charContainer);
 document.getElementsByClassName("container")[0].appendChild(unicodePal);
+const characters = [
+  ["Blocks", "█■▔🭶🭷🭸🭹🭺🭻▁🮀🮁🮗▐▌▞▚▛▀▜▙▄▟▗▖▝▘🮕🮖░▒▓🮐🮒🮑🮘🮙▕🮇🮈🮉🮊🮋▉▊▋▍▎▏▂▃▅▆▇🮆🮅🮄🮃🮂🭽🭾🭼🭿"],
+  ["Slopes", "🮞🮟🮝🮜🭈🭆🭂🭍🭑🬽🭣🭧🭓🭞🭜🭘🭊🭁🭇🭄🭏🬼🭌🬿🭥🭒🭢🭕🭠🭗🭝🭚◢◣◥◤🭃🭉🬾🭎🭔🭤🭙🭟🭅🭋🭀🭐🭖🭦🭛🭡🭮🭩🭯🭨🮛🭪🭬🭫🮚🭭"],
+  ["Sextants", "🬞🬭🬏🬇🬋🬃🬁🬂🬀🬠🬰🬐🬦🬹🬓🬉🬎🬄🬙🬥🬖🬢🬔🬧🬡🬒🬜🬪🬶🬳🬈🬅🬣🬘🬯🬮🬛🬫🬴🬸🬤🬗🬕🬨🬚🬩🬝🬬🬵🬱🬲🬷🬌🬍🬺🬻🬊🬆"],
+  ["Box Drawing", "━┃╺╸╻╹═║╌┄┈╍┅┉╎┆┊╏┇┋┍┯┑┝┿┥┕┷┙╒╤╕╞╪╡╘╧╛╓╥╖╟╫╢╙╨╜╔╦╗╠╬╣╚╩╝╲╳╱🮭🮬🮫🮪🮣🮢🮡🮠🮦🮥🮮🮤🮧🮩🮨"],
+  ["Arrows", "←↑→↓↖↗↙↘⤡⤧⇆⤨⇅⇵⇄⤩⤪"],
+  ["Miscellaneous", "□◄▲►▼🮵🮶🯄🮱🮰🮷🮸🮴🮽🮾🮿🮹🮺🮻🯊🮼🯀🯁🯂🯃🯅🯆🯇🯈🮲🮳🯉🯰🯱🯲🯳🯴🯵🯶🯷🯸🯹⎞⎝⎜⎛⎠⎞"]
+];
+let currentGroup = 0;
+function showGroup(id) {
+  let c = characters[id][1].split(/(?:)/u);
+  let l = document.getElementById("chars");
+  while (l.hasChildNodes()) {
+    l.removeChild(l.firstChild);
+  }
+  for (let i in c) {
+    let btn = document.createElement("button");
+    btn.addEventListener("click", ()=>{writeChar(c[i], 1)});
+    btn.innerHTML = c[i];
+    document.getElementById("chars").appendChild(btn)
+  };
+  document.getElementById("groupName").innerHTML = characters[id][0]
+};
+function fmod(a, b) { return ((a % b) + b) % b };
+document.getElementById("prevGroup").addEventListener("click", ()=>{
+  currentGroup = fmod(currentGroup - 1, characters.length);
+  showGroup(currentGroup);
+});
+document.getElementById("nextGroup").addEventListener("click", ()=>{
+  currentGroup = fmod(currentGroup + 1, characters.length);
+  showGroup(currentGroup);
+});
+showGroup(currentGroup);
