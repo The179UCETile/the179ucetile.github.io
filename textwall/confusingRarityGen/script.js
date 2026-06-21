281eab69ef96b29b11b039169780bf0edf8052ee
The179UCETile <khang240582@gmail.com>
2026-06-21 15:52:01 +0700

    Update script.js

--- textwall/confusingRarityGen/script.js
+++ textwall/confusingRarityGen/script.js
@@ -1,15 +1,18 @@
 const prefixes = "gran,ex,met,supi,suter,versa,omn,hypi,etern,parad,plexi,dy,try,ad,en,scend,di,tri,dic,asc,sup,grea,xi,colos,ginorma,fiz,omeg,termin,lute,mil,bil,tril,quadr,micr,nan,pic,femt,kal,multi,terra,jova,gargantua,hysu,zenna,aetern,entrop,absur,phos,sud,norik,epusorik,ultimik,finth,gendr,th,memori,metro,mej,gi,ter,pet,exa,zet,yot,ron,que,incarn,ate,monstructa,maxim,demi,quasi,ulti,ji,ti,ter,petri,att,zept,yoct,ront,quec,pro,anti,tetrat,endian,us,pulmen,fino,exe,kiaro,ikai,opul,era,gen,kian,brony,renk,asyak,mendr,broth,ork,zy,extra,trances,ab,prem,fundam,trundam,undam,izer,zy,alp,bet,gam,del,psi,sort,conja,uni,dua,imag,nihil,neg,absil,zyro,expanta,eterna,minim,ubera,eka,disa,trisa,plusi,then,cryo,ployt,on,byzr,ol,realit,tayer,ziren,iren,tri,tetra".split(","),
   endings = "ia,ium,us,al,gen,ite,lic,cate,um,on,io,iko,o,try,rum,ion,oes,ic,uum,a,e,i,u,ol,am,is,finity".split(","), // -Num overflow is treated differently
   startings = "THE START OF ,THE END OF ,Post-,Limit of ".split(","),
-  doubVowels = [["aa", "aia"],["ee", "ete"],["ii", "ini"],["oo", "ono"],["uu", "utu"]];
+  doubVowels = ["aa", "ee", "ii", "oo", "uu"];
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
 function generateRarityWord(prefs) {
   let output = "";
   for (let i = 0; i < prefs; i++) {
-    output += prefixes[Math.floor(Math.random() * prefixes.length)]
+    output += randomItem(prefixes);
   };
-  output += endings[Math.floor(Math.random() * endings.length)]
+  output += randomItem(endings)
   for (let i of doubVowels) {
-    output = output.replace(new RegExp(`${i[0]}`, "g"), i[1]);
+    output = output.replace(new RegExp(`${i}`, "g"), i[0] + randomItem("bcdfghjklmnpqrstvwxyz") + i[1]);
   };
   return output[0].toUpperCase() + output.slice(1)
 }
